5a7426205affa2691144cd35a15a37e45056224f
James Gary <gmarchesi888@gmail.com>
2024-12-28 11:38:04 -0500

    First commit.

---
+++ README.md
@@ -0,0 +1,9 @@
+This is an extremely simple Python program that uses machine learning to predict the likelihood that an individual from a particular population will have diabetes.  The actual code is pretty much cut-and-pasted from here:
+
+https://plainenglish.io/blog/how-to-build-a-machine-learning-model-from-scratch-with-python
+
+And the data in the diabetes.csv file is from here:
+
+https://www.kaggle.com/datasets/mathchi/diabetes-data-set?resource=download
+
+Note that you will need to install the "pandas" and "sklearn" (or maybe "scikit-learn"-the name seems to have been changed) for the app to run successfully.
---
+++ app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+
+data = pd.read_csv("diabetes.csv")
+scaler = StandardScaler()
+model = LogisticRegression()
+
+# Separate the features (X) and the target variable (y)
+X = data.drop("Outcome", axis=1) # Features (independent variables)
+y = data["Outcome"] # Target (dependent variable)
+X_scaled = scaler.fit_transform(X)  # normalize the imported data to 1(?)
+
+# Split the data into training and test sets (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(
+    X_scaled, y, test_size=0.2, random_state=42
+)
+
+# Train the model
+model.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = model.predict(X_test)
+
+# Calculate the accuracy
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Model Accuracy: {accuracy * 100:.2f}%")
+
+new_patient = [[1.5, 85, 66, 29, 0, 26.6, 0.351, 31]] # new row of data for a new patient
+
+# Predict the outcome (0 = no diabetes, 1 = diabetes)
+prediction = model.predict(new_patient)
+
+print(f"Diabetes Prediction: {prediction[0]}")
