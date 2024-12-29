84ca69d48f4df3fc5a1ab2ed284babf72117548a
James Gary <gmarchesi888@gmail.com>
2024-12-29 16:08:01 -0500

    Added 'outcome zero' test case.

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ scaler = StandardScaler()
 model = LogisticRegression()
 
 # Separate the features (X) and the target variable (y)
-X = data.drop("Outcome", axis=1) # Features (independent variables)
-y = data["Outcome"] # Target (dependent variable)
+X = data.drop("Outcome", axis=1)  # Features (independent variables)
+y = data["Outcome"]  # Target (dependent variable)
 X_scaled = scaler.fit_transform(X)  # normalize the imported data to 1(?)
 
 # Split the data into training and test sets (80% train, 20% test)
@@ -29,9 +29,18 @@ y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f"Model Accuracy: {accuracy * 100:.2f}%")
 
-new_patient = [[1.5, 85, 66, 29, 0, 26.6, 0.351, 31]] # new row of data for a new patient
+new_patient = [
+    [1.5, 85, 66, 29, 0, 26.6, 0.351, 31]
+]  # new row of data for a new patient - prediction 1
+
+outcome_zero_patient = [
+    [8, 99, 84, 0, 0, 35.4, 0.388, 50]
+]  # new row of data for a new patient - prediction 0
+
 
 # Predict the outcome (0 = no diabetes, 1 = diabetes)
 prediction = model.predict(new_patient)
+prediction2 = model.predict(outcome_zero_patient)
 
-print(f"Diabetes Prediction: {prediction[0]}")
+print(f"Diabetes Prediction (yes): {prediction[0]}")
+print(f"Diabetes Prediction (no): {prediction2[0]}")
